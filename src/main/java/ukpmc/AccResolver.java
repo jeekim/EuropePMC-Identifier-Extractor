c4ec18cdceaa98ccb2a60d21971098d457aaaa92
jee <jee@jhkim@ebi.ac.uk>
2015-11-19 17:55:34 +0000

    from soap to rest

---
+++ src/main/java/ukpmc/AccResolver.java
@@ -0,0 +1,69 @@
+package ukpmc;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class AccResolver implements IDResolver {
+   private String host = "www.ebi.ac.uk";
+   // private String host = "www.ebi.ac.uk/ebisearch/ws/rest";
+   private int port = -1;
+
+   private URL toURL(String doi) {
+      try {
+         URL url = new URL("http", host, port, 
+           '/' + doi.replaceAll("#", "%23").replaceAll("\\[", "%5B").replaceAll("\\]", "%5D"));
+         return url;
+      } catch (MalformedURLException e) {
+         throw new IllegalArgumentException();
+      }
+   }
+
+   private String toString(URL url) {
+      String doi = url.getPath().substring(1).replaceAll("%23", "#").replaceAll("%5B", "\\[").replaceAll("%5D", "\\]");
+      return doi;
+   }
+
+   /* public boolean isValidID(String doi) {
+      try {
+         URL url = toURL(doi);
+	 System.err.println(url);
+         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+         String response = connection.getResponseMessage();
+         connection.disconnect();
+         if (response.equals("OK")) {
+            return true;
+         } else {
+            return false;
+         }
+      } catch (MalformedURLException e) {
+         throw new RuntimeException(e);
+      } catch (IOException e) {
+         throw new RuntimeException(e);
+      }
+   } */
+
+   public boolean isValidID(String doi)  {
+     try {
+       URL url = toURL(doi);
+       BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+       String inputLine;
+
+       while ((inputLine = in.readLine()) != null) {
+         System.err.println("+++" + inputLine + "+++");
+	 if (inputLine.contains("<hitCount>0</hitCount>")) {
+		 return false;
+         } 	
+       }
+       in.close();
+     } catch (IOException e) {
+        ;
+     }
+     return true;
+  }
+}
